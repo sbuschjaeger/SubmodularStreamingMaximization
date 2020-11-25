9d2f21cc2740a6c8547352ceb6d8821bf9f775d1
Lukas Pfahler <lukas.pfahler@udo.edu>
2020-11-25 12:04:39 +0100

    Stream51

--- experiments/stream51/init.sh
+++ experiments/stream51/init.sh
@@ -3,11 +3,13 @@
 git clone https://github.com/tyler-hayes/Stream-51
 
 # download video frames
-gdown --id "15huZ756N2cp1CCO4HxF-MVDsMx1LMoIn" --output Stream51.zip
+echo "Trying to download https://drive.google.com/file/d/15huZ756N2cp1CCO4HxF-MVDsMx1LMoIn/view?usp=sharing"
+echo "Should this fail, to so manually and place at $( pwd )"
+gdown --id "15huZ756N2cp1CCO4HxF-MVDsMx1LMoIn" --output Stream-51.zip
 
 # extract video frames from zip
-mkdir "data"
-unzip Stream51.zip -d "data"
+mkdir -p "data"
+unzip Stream-51.zip -d "data"
 
 # extract feature vectors with pytorch and InceptionV3
 python init.py
