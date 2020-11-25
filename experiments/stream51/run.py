76f925e34c91f7de2b9ecd784c89defcbcdc865e
Lukas Pfahler <lukas.pfahler@udo.edu>
2020-11-25 11:37:26 +0100

    run file for Stream51 uses relative data path

--- experiments/stream51/run.py
+++ experiments/stream51/run.py
@@ -89,7 +89,7 @@ def post(cfg, opt):
     }
 
 print("Loading data")
-X = np.load("/data/s1/buschjae/SubmodularStreamingMaximization/stream51.npy")
+X = np.load("data/stream51.npy")
 
 min_max_scaler = preprocessing.Normalizer()
 X = min_max_scaler.fit_transform(X)
